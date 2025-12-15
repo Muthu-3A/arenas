ef3c9cfe48bc63342b3de8f8c5ef0dd0bd916ac1
Muthu-3A <muthulingah@gmail.com>
2025-12-15 14:09:48 +1300

    todos filled out for basic implementation

--- arenas2.c
+++ arenas2.c
@@ -7,6 +7,8 @@
 #define PushStruct(arena, type) PushArray((arena), (type), 1)
 #define PushStructZero(arena, type) PushArrayZero((arena), (type), 1)
 
+#define ArenaDefaultCapacity (1024 * 1024) //1 megabyte
+
 
 typedef struct{
     unsigned char *base;
@@ -27,14 +29,84 @@ void ArenaGetPosBack(Arena *arena, size_t pos);
 void ArenaClear(Arena *arena);
 
 Arena *ArenaAlloc(void) {
-    //todo
+    Arena *arena = malloc(sizeof *arena);
+    if(!arena) {
+        return NULL; //allocation failed
+    }
+
+    arena->base = malloc(ArenaDefaultCapacity);
+    if(!arena->base) {
+        free(arena);
+        return NULL; //allocation failed
+    }
+
+    arena->capacity = ArenaDefaultCapacity
+    arena->offset = 0;
+
+    return arena
 }
 
 void ArenaRelease(Arena *arena) {
     free(arena->base);
 }
 
+void *ArenaPush(Arena *arena, size_t size) {
+    if(!arena || size == 0) {
+        return NULL; //invalid function call
+    }
+
+    if(arena->capacity - arena->offset < size) {
+        return NULL; //cannot allocate required memory
+    }
+
+    void *ptr = arena->base + arena->offset;
+    arena->offset += size;
+    return ptr
+}
+
+void *ArenaPushZero(Arena *arena, size_t size) {
+    if(!arena || size == 0) {
+        return NULL;
+    }
+
+    if(arena->capacity - arena->offset < size) {
+        return NULL;
+    }
+
+    void *ptr = arena->base + arena->offset;
+    arena->offset += size;
+
+    memset(ptr, 0, size); //zero the allocated memory
+    return ptr
+}
+
+void ArenaPop(Arena *arena, size_t size) {
+    if(!arena || size == 0) {
+        return NULL;
+    }
+
+    if(size > arena->offset) {
+        //could set offset to 0 but want concious ArenaClear to be used for that
+        return NULL;  // so in this implementation we catch arena accounting mistakes
+    }
+    
+    arena->offset -= size;  
+}
+size_t ArenaGetPos(Arena *arena) {
+    if(!arena) return;
+    return arena->offset;
+}
+
+void ArenaGetPosBack(Arena *arena, size_t pos) {
+    if(!arena || pos > arena->offset) {
+        return; //invalid
+    }
+
+    arena->offset = pos;
+}
+
 void ArenaClear(Arena *arena) {
+    if(!arena) return;
     arena->offset = 0;
 }
 
